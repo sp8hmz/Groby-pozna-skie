3de83e25834148c9d521e762cafd88a21453016b
Karol Karczewski <sp8hmz@wp.pl>
2017-05-16 15:56:43 +0200

    Implemented searching function, small fixes in UI and icon graphics.

--- Groby poznańskie/ListViewController.swift
+++ Groby poznańskie/ListViewController.swift
@@ -10,19 +10,33 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 import MapKit
+import KRProgressHUD
 
-class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var isSearching = false
     
     var graves = [Grave]()
+    var filteredGraves = [Grave]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.done
+        searchBar.setValue("Anuluj", forKey:"cancelButtonText")
         getData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let row = tableView.indexPathForSelectedRow {
+            self.tableView.deselectRow(at: row, animated: false)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,10 +44,10 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func getData() {
-        Alamofire.request("http://www.poznan.pl/featureserver/featureserver.cgi/groby/all.json?maxfeatures=100").validate().responseData { response in
+        KRProgressHUD.show(message: "Ładowanie")
+        Alamofire.request("http://www.poznan.pl/featureserver/featureserver.cgi/groby/all.json?maxfeatures=200").validate().responseData { response in
             if let _ = response.result.value {
                 let parsedJSON = JSON(data: response.result.value!)
-                
                 for i in 0..<parsedJSON["features"].count {
                     var nameAndSurname = parsedJSON["features"][i]["properties"]["print_surname_name"].stringValue
                     let name = parsedJSON["features"][i]["properties"]["print_name"].stringValue
@@ -52,13 +66,18 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
                     let quarter = parsedJSON["features"][i]["properties"]["g_quarter"].stringValue
                     let row = parsedJSON["features"][i]["properties"]["g_row"].intValue
                     let place = parsedJSON["features"][i]["properties"]["g_place"].intValue
-                    
                     let grave = Grave(nameAndSurname: nameAndSurname, name: name, surname: surname, birthDate: birthDate!, deathDate: deathDate!, id: id, coordinates: coordinates, quarter: quarter, row: row, place: place)
                     self.graves.append(grave)
                 }
                 self.tableView.reloadData()
+                KRProgressHUD.dismiss()
             } else {
-                //Error
+                KRProgressHUD.dismiss()
+                let alertController = UIAlertController(title: "Error", message:
+                    "Wystąpił błąd podczas pobierania danych z serwera. Spróbuj ponownie.", preferredStyle: UIAlertControllerStyle.alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: nil))
+                
+                self.present(alertController, animated: true, completion: nil)
             }
         }
     }
@@ -69,25 +88,49 @@ class ListViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return graves.count
+        if isSearching {
+            return filteredGraves.count
+        } else {
+            return graves.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ListTableCell
-        
-        let grave = graves[indexPath.row]
-
-        cell.nameAndSurnameTextLabel.text = grave.nameAndSurname
+        let grave: Grave?
+        if isSearching {
+            grave = filteredGraves[indexPath.row]
+        } else {
+            grave = graves[indexPath.row]
+        }
+        cell.nameAndSurnameTextLabel.text = grave!.nameAndSurname
         return cell
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetails" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let grave = graves[indexPath.row]
+                let grave: Grave?
+                if isSearching {
+                    grave = filteredGraves[indexPath.row]
+                } else {
+                    grave = graves[indexPath.row]
+                }
                 let controller = segue.destination as! DetailsViewController
-                controller.grave = grave
+                controller.grave = grave!
             }
+        }
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" || searchBar.text?.isEmpty == true {
+            isSearching = false
+            view.endEditing(true)
+            tableView.reloadData()
+        } else {
+            isSearching = true
+            filteredGraves = graves.filter({$0.nameAndSurname.contains(searchBar.text!)}    )
+            tableView.reloadData()
         }
     }
 
